583f1ed4c067d4312ddb2561aa173b38bb68ad54
Ansoumane Kadher Kande (Liam) <liamkande@Liams-MacBook-Pro.local>
2023-04-19 15:39:42 -0700

    refactored accuracy report

--- score.js
+++ score.js
@@ -5,26 +5,35 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
-const predictionPoint = 300;
-const k = 3;
+const splitDataset = (data, testCount) => {
+  const shuffled = _.shuffle(data);
 
-const runAnalysis = () => {
-  const testSize = 10
-  const [testSet, trainngSet] = splitDataset(outputs, 10);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
 
-  let numberCorrect = 0
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainngSet, testSet[i][0]);
-    if (bucket === testSet[i][3]) {
-      numberCorrect++
-    }
-  }
-  console.log('Accuracy:', numberCorrect / testSize);
+  return [testSet, trainingSet];
 };
 
-const knn = (data, point) => {
-  return _.chain(outputs)
-    .map((row) => [distance(row[0]), row[3]])
+const runAnalysis = () => {
+  const testSetSize = 10;
+  const [testSet, trainngSet] = splitDataset(outputs, 10);
+
+  _.range(1, 15).forEach((k) => {
+    const accuracy = _.chain(testSet)
+      .filter((testPoint) => knn(trainngSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log("For k of", k, "accuracy is:", accuracy);
+  });
+};
+
+const distance = (pointA, pointB) => Math.abs(pointA - pointB);
+
+const knn = (data, point, k) => {
+  return _.chain(data)
+    .map((row) => [distance(row[0], point), row[3]])
     .sortBy((row) => row[0])
     .slice(0, k)
     .countBy((row) => row[1])
@@ -34,15 +43,4 @@ const knn = (data, point) => {
     .first()
     .parseInt()
     .value();
-};
-
-const distance = (pointA, pointB) => Math.abs(pointA - pointB);
-
-const splitDataset = (data, testCount) => {
-  const shuffled = _.shuffle(data);
-
-  const testSet = _.slice(shuffled, 0, testCount);
-  const trainingSet = _.slice(shuffled, testCount);
-
-  return [testSet, trainingSet];
 };
