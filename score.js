d08833fb5df12e291e607bb51670b71c5c37e9f4
Ansoumane Kadher Kande (Liam) <liamkande@Liams-MacBook-Pro.local>
2023-04-28 17:58:55 -0700

    added minMax function

--- score.js
+++ score.js
@@ -1,49 +1,65 @@
-const outputs = [];
+const outputs = []
 
 const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => {
   // Ran every time a balls drops into a bucket
-  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  outputs.push([dropPosition, bounciness, size, bucketLabel])
 }
 
 const splitDataset = (data, testCount) => {
-  const shuffled = _.shuffle(data);
+  const shuffled = _.shuffle(data)
 
-  const testSet = _.slice(shuffled, 0, testCount);
-  const trainingSet = _.slice(shuffled, testCount);
+  const testSet = _.slice(shuffled, 0, testCount)
+  const trainingSet = _.slice(shuffled, testCount)
 
-  return [testSet, trainingSet];
-};
+  return [testSet, trainingSet]
+}
+
+const minMax = (data, featureCount) => {
+  const clonedData = _.cloneDeep(data)
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map((row) => row[i])
+
+    const min = _.min(column)
+    const max = _.max(column)
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min)
+    }
+  }
+}
 
 const runAnalysis = () => {
-  const testSetSize = 10;
-  const [testSet, trainngSet] = splitDataset(outputs, 10);
-
+  const testSetSize = 10
+  const [testSet, trainngSet] = splitDataset(outputs, 10)
+ 
   _.range(1, 11).forEach((k) => {
     const accuracy = _.chain(testSet)
-      .filter((testPoint) => knn(trainngSet, _.initial(testPoint), k) === testPoint[3])
+      .filter(
+        (testPoint) => knn(trainngSet, _.initial(testPoint), k) === testPoint[3]
+      )
       .size()
       .divide(testSetSize)
-      .value();
+      .value()
 
-    console.log("For k of", k, "accuracy is:", accuracy);
-  });
-};
+    console.log("For k of", k, "accuracy is:", accuracy)
+  })
+}
 
 const distance = (pointA, pointB) => {
-  return _.chain(pointA)
-  .zip(pointB)
-  .map(([a, b]) => (a - b) ** 2)
-  .sum()
-  .value() ** 0.5
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  )
 }
 
 const knn = (data, point, k) => {
   return _.chain(data)
     .map((row) => {
-      return [
-        distance(_.initial(row), point), 
-        _.last(row)
-      ]
+      return [distance(_.initial(row), point), _.last(row)]
     })
     .sortBy((row) => row[0])
     .slice(0, k)
@@ -53,6 +69,5 @@ const knn = (data, point, k) => {
     .last()
     .first()
     .parseInt()
-    .value();
-};
-
+    .value()
+}
