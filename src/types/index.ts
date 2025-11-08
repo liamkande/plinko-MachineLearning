2bdfb15cf305457ec056c3a16e7b027e07ea7a78
Liam Ansoumane Kande <liamkande@icloud.com>
2025-11-07 22:38:27 -0800

    creates type file

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface BallData {
+  dropPosition: number;
+  bounciness: number;
+  size: number;
+  bucketLabel: number;
+}
+
+export interface AnalysisResult {
+  feature: number;
+  featureName: string;
+  accuracy: number;
+}
+
+export interface PlinkoConfig {
+  coefStart: number;
+  coefEnd: number;
+  sizeStart: number;
+  sizeEnd: number;
+  dropQuantity: number;
+}
